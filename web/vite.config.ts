d8433d963749a95559a93ebaed27f8be309b7f95
Victor Zavala <victor.zavala@Mac.fritz.box>
2026-04-24 16:52:39 +0200

    Update vite to get env variables from root, add MUI dependencies

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  envDir: path.resolve(__dirname, '..'),
 });
